2783a718c0bee90e4822f47b01d5cfdabbfbf972
Zafarullah Naushad <thezafar.m3n@gmail.com>
2023-06-11 16:12:18 +0530

    Eighth commit- Doctor user model

---
+++ models/doctorModel.js
@@ -0,0 +1,49 @@
+const mongoose = require("mongoose");
+
+const doctorSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+    },
+    firstName: {
+      type: String,
+      required: [true, "First name is required"],
+    },
+    lastName: {
+      type: String,
+      required: [true, "Last name is required"],
+    },
+    nic: {
+      type: String,
+      required: [true, "NIC is required"],
+    },
+    phone: {
+      type: String,
+      required: [true, "Phone number is required"],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+    },
+    address: {
+      type: String,
+      required: [true, "Address is required"],
+    },
+    specialisation: {
+      type: String,
+      required: [true, "Specialisation is required"],
+    },
+    fees: {
+      type: number,
+      required: [true, "Fee is required"],
+    },
+    timings: {
+      type: Object,
+      required: [true, "Available times required"],
+    },
+  },
+  { timestamps: true }
+);
+
+const doctorModel = mongoose.model("users", doctorSchema);
+module.exports = doctorModel;
