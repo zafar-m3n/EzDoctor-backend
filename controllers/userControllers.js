10afce96152081e4d5e743ea8e4cf35fc3779942
Zafarullah Naushad <thezafar.m3n@gmail.com>
2023-06-11 11:39:23 +0530

    Fifth commit- login controller

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -28,6 +28,31 @@ const registerController = async (req, res) => {
 };
 
 // Login Callback
-const loginController = () => {};
+const loginController = async (req, res) => {
+  try {
+    const user = await userModel.findOne({ email: req.body.email });
+    if (!user) {
+      return res
+        .status(200)
+        .send({ message: "User Not Found", success: false });
+    }
+    const isMatch = await bcrypt.compare(req.body.password, user.password);
+    if (!isMatch) {
+      return res
+        .status(200)
+        .send({ message: "Invalid Email or Password", success: false });
+    }
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1d",
+    });
+    res.status(200).send({ message: "Login Success", success: true, token });
+  } catch (error) {
+    console.log(error);
+    res
+      .status(500)
+      .send({ message: `Error in Login Control ${error.message}` });
+  }
+};
 
 module.exports = { loginController, registerController };
