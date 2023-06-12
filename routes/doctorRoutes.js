32cbfdbf451d0a0b2ac7c9d07ae4c33ddae38fb8
Zafarullah Naushad <thezafar.m3n@gmail.com>
2023-06-12 16:07:57 +0530

    16th commit- Update profile controller and route

--- routes/doctorRoutes.js
+++ routes/doctorRoutes.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const authMiddleware = require("../middlewares/authMiddleware");
-const { getDoctorInfoController } = require("../controllers/doctorControllers");
+const {
+  getDoctorInfoController,
+  updateProfileController,
+} = require("../controllers/doctorControllers");
 
 const router = express.Router();
 
 // Get a doctor || POST
 router.post("/getDoctorInfo", authMiddleware, getDoctorInfoController);
+
+// Update Profile || POST
+router.post("/updateProfile", authMiddleware, updateProfileController);
 
 module.exports = router;
